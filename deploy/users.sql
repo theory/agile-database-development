ead1c70ff444ee2257a24fd188409c1698883a29
David E. Wheeler <david@justatheory.com>
2014-01-07 20:44:24 -0800

    Add users table.

---
+++ deploy/users.sql
@@ -0,0 +1,13 @@
+-- Deploy users
+-- requires: appschema
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+CREATE TABLE flipr.users (
+    nickname  TEXT        PRIMARY KEY,
+    password  TEXT        NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+COMMIT;
